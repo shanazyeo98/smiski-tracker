9bcc9ad43a08ad69d8c50808d92624598cd5e694
Shanaz <153274702+shanazyeo98@users.noreply.github.com>
2025-09-16 23:11:36 +0800

    trying to render the categories in the collectionview

---
+++ smiski tracker/Custom Views/CategoryCell.swift
@@ -0,0 +1,12 @@
+//
+//  CategoryCell.swift
+//  smiski tracker
+//
+//  Created by Shanaz Yeo on 16/9/25.
+//
+
+import UIKit
+
+class CategoryCell: UICollectionViewCell {
+    
+}
---
+++ smiski tracker/Utilities/UIHelper.swift
@@ -0,0 +1,8 @@
+//
+//  UIHelper.swift
+//  smiski tracker
+//
+//  Created by Shanaz Yeo on 16/9/25.
+//
+
+import Foundation
